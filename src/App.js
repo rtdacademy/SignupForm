d71d86274a92545992da83bb423adef9b30b3a78
Kyle Brown <128441953+rtdacademy@users.noreply.github.com>
2024-07-19 14:20:25 +0000

    Get Main Back

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationForm";
+import "./styles/styles.css"; // If you have any global styles
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
