7b712644e92623bfe70cf7c7e0a2a7885f53ac8a
rtdacademy <kyle@rtdacademy.com>
2025-06-07 19:44:19 -0600

    feat: Import useState for managing component state in WelcometoRTDAcademy

--- src/FirebaseCourses/courses/4/content/01-welcome-rtd-academy/index.js
+++ src/FirebaseCourses/courses/4/content/01-welcome-rtd-academy/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AIMultipleChoiceQuestion } from '../../../../components/assessments';
 
 const WelcometoRTDAcademy = ({ courseId }) => {
